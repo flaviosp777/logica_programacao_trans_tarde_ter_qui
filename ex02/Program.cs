a426c644adee609f2a4348c0601460fa336f43d5
FLAVIO JOSE CARDOSO DE JESUS <flavio.jcjesus@senacsp.edu.br>
2025-01-14 15:23:46 -0300

    first commit

---
+++ ex02/Program.cs
@@ -0,0 +1,3 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Olá mundo! Sou Flávio");
+Console.WriteLine("Estou aprendendo lógica de programação");
---
+++ ex03/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Olá mundo! Sou o Flávio");
+Console.WriteLine("Estou estudando no Senac Largo 13");
+
